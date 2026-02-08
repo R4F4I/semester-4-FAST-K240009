3ed7cfc63f239255896e525a25e35d2d08f7705f
R4F4I <your_email@example.com>
2026-02-08 19:57:44 +0500

    changed q5

--- OS/lab/assigments/lab3/q5.sh
+++ OS/lab/assigments/lab3/q5.sh
@@ -2,8 +2,11 @@
 folder=$1
 current_date=$(date +%Y-%m-%d)
 backup_name="project_backup_"$current_date
+
 mkdir $backup_name
-cp $folder/* $backup_name
+
+cp -r $folder/* $backup_name 2>/dev/null
+
 count=$(ls $backup_name | wc -l)
 echo "Files backed up: $count"
 echo "Backup successful"
